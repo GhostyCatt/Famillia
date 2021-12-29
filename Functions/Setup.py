7f270bf94bea808776ee2aa23fc4fee73117900d
Ghostyy 5668 <cosvicx9126@gmail.com>
2021-12-29 17:13:02 +0530

    📦 NEW: Jishaku Integration

--- Functions/Setup.py
+++ Functions/Setup.py
@@ -14,7 +14,7 @@ def setupBot(bot: commands.Bot, config_data: dict):
         exit()
 
     # Config Validity
-    if config_data['Roles'] or config_data['Users'] == None: 
+    if config_data['Roles'] == None or config_data['Users'] == None: 
         print("ALERT: The config file isn't fully setup!")
         exit()
 
@@ -23,7 +23,10 @@ def setupBot(bot: commands.Bot, config_data: dict):
     botdata["Runs"] += 1
     with open("Data/Bot.json", "w") as writable_botdata: json.dump(botdata, writable_botdata, indent = 4)
 
+    try: bot.load_extension('jishaku')
+    except Exception as e: print(e)
+
     for filename in os.listdir("Modules/"):
         if filename.endswith(".py"):
             try: bot.load_extension(f"Modules.{filename[:-3]}")
-            except: pass
+            except Exception as error: print(error)
